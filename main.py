cb7f822a89c072330903d6ea4bbe2c4979ce0da8
Picola555 <petecolasacco@hotmail.com>
2025-12-25 14:37:04 -0800

    Boot.dev asteroids class

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from logger import log_state
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #main loop
+    while True:
+	# make the close button work
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()  
+
+if __name__ == "__main__":
+    main()
